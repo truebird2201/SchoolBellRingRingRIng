827a18ff22c93663d28c1b216bc77bfa77ad634e
이서연 <lail>
2022-05-13 00:37:16 +0900

    거의 다 구현

--- game/Connect_Four.py
+++ game/Connect_Four.py
@@ -1,8 +1,14 @@
 from tkinter import *
-
+from tkinter import font
 from setuptools import Command
+
 _MAXROW = 6
 _MAXCOL = 7
+Turn="red"
+cells=[]
+restart_Text = "새로 시작"
+process_button=None
+fin = False
 
 class Cell(Canvas):
     global cells
@@ -20,11 +26,19 @@ class Cell(Canvas):
         self.color = color
         self.create_oval(4, 4, 20, 20, fill = self.color, tags="oval")
 
-    def clicked(self,event): # red 또는 yellow 돌 놓기.
-        nextcolor = "red" if self.color != "red" else "yellow"
-        self.setColor(nextcolor)
+    def setbg(self, bg):
+        self.delete("oval")
+        self.bg = bg
+        self.create_oval(4, 4, 20, 20, fill = self.color, tags="oval")
 
-        self.Check()
+    def clicked(self,event): # red 또는 yellow 돌 놓기.
+        if ((((self.row!=_MAXROW-1) and cells[(self.row+1)][self.col].color!="white") or self.row==_MAXROW-1) and self.color == "white") and fin==False:
+            
+            global Turn
+            self.setColor(Turn)
+            self.Check()
+            Turn = "red" if self.color != "red" else "yellow"
+            
     
     def __CheckVertical():
         for r in range(0, _MAXROW):
@@ -47,6 +61,7 @@ class Cell(Canvas):
         return False
 
     def __CheckDiag1():
+        global Turn
         for r in range(0, _MAXROW):
             for c in range(0, _MAXCOL):
                 if r + 3 < _MAXROW and c + 3 < _MAXCOL and \
@@ -54,10 +69,15 @@ class Cell(Canvas):
                     cells[r][c].color == cells[r+2][c+2].color and \
                     cells[r][c].color == cells[r+3][c+3].color and \
                     cells[r][c].color != "white":
+                    cells[r][c].setbg("white")
+                    cells[r+1][c+1].setbg(Turn)
+                    cells[r+2][c+2].setbg(Turn)
+                    cells[r+3][c+3].setbg(Turn)
                     return True
         return False
 
     def __CheckDiag2():
+        global Turn
         for r in range(0, _MAXROW):
             for c in range(0, _MAXCOL):
                 if r + 3 < _MAXROW and c - 3 > 0 and \
@@ -65,24 +85,36 @@ class Cell(Canvas):
                     cells[r][c].color == cells[r+2][c-2].color and \
                     cells[r][c].color == cells[r+3][c-3].color and \
                     cells[r][c].color != "white":
+                    cells[r][c].setbg("white")
+                    cells[r+1][c-1].setbg(Turn)
+                    cells[r+2][c-2].setbg(Turn)
+                    cells[r+3][c-3].setbg(Turn)
                     return True
         return False
 
     def Check(self):
         if (Cell.__CheckVertical() or Cell.__CheckHorizontal() or Cell.__CheckDiag1() or Cell.__CheckDiag2()):
             print(True)
+            process_button.configure(text=Turn+"의 승리 ! ")
+            global fin
+            fin=True
             
 
-def press(list , bt):
-    if (bt['text'] == "새로 시작"):
-        reset(list)
+def press():
+    if (process_button['text'] == "새로 시작"):
+        global fin, Turn
+        fin=False
+        Turn = "red"
+        reset()
     else:
-        bt.configure(text="새로 시작")
+        process_button.configure(text="새로 시작")
 
-def reset(list):
+def reset():
+
     for r in range(_MAXROW):
         for c in range(_MAXCOL):
-            list[r][c].setColor("white")
+            cells[r][c].setColor("white")
+            
     
 
 
@@ -91,12 +123,18 @@ window.title("Connect Four") # Set title
 
 frame1 = Frame(window)
 frame1.pack()
-frame2 = Frame(window)
-frame2.pack()
+
+gameframe = Frame(window)
+gameframe.pack()
+
+frameResult = Frame(gameframe,padx=12, pady=4, bg='#ffc4c4')                            # 결과 프레임
+frameResult.pack(side="bottom",fill="both")
+
 cells = [[Cell(frame1, r, c, width = 20, height = 20) for c in range(_MAXCOL)] for r in range(_MAXROW)]
-botton = Button(frame2,text="새로 시작",width=10,height=2)
-botton.configure(command=lambda list=cells, b=botton : press(list, b))
-botton.grid(row=0,column=0)
+fontTitle = font.Font(window, size=10, weight='bold', family = '윤고딕230')
+
+process_button = Button(frameResult,command=press,width=20, font = fontTitle, text=restart_Text,bg='#fff8dd')
+process_button.pack(anchor="center", fill="both")
 
 for cell in cells:
     for c in cell:
