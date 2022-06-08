6d4a8cbcf7f4bb6bd04ef3ca87d42b5bc2d7eeca
이서연 <lail>
2022-06-09 00:02:08 +0900

    집파일

---
+++ project/spam/spam/spammodule.c
@@ -0,0 +1,10 @@
+#include <python.h>
+#include <string.h>
+static PyObject* spam_strlen(PyObject* self, PyObject* args) {
+	char* str;
+	int len;
+	if (!PyArg_ParseTuple(args, "s", &str))
+		return NULL;
+	len = strlen(str);
+	return Py_BuildValue("i", len);
+}
