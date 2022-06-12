728d3e6582e7a3f802b7824bca0d71de690713bf
이서연 <lail>
2022-06-12 19:24:14 +0900

    셋업

---
+++ project/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup, Extension
+
+module_spam = Extension
+setup(
+    name = 'School_Bell_Ring',
+    version='1.0',
+    py_modules=['School_Bell_Ring'],
+    packages=['image'],
+)
